2fa965527d396db8371c2fdf69f5113f2a440b26
Camarederic <camarederic@gmail.com>
2025-06-22 22:50:47 +0300

    added hamburger and adaptation for the section header

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,6 @@
-$(function () {
-  
+const menuBtn = document.querySelector(".menu__btn");
+const menu = document.querySelector(".menu__list");
+
+menuBtn.addEventListener("click", () => {
+  menu.classList.toggle("menu--active");
 });
